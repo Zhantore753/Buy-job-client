8bd48382bb89df8edc7db3999b0b67575a69a8ae
Zhantore <753gg@mail.ru>
2021-04-17 14:55:18 +0600

    redux

--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {createStore} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import {Provider} from "react-redux";
+import {rootReducer} from './reducers/rootReducer';
+
+const store = createStore(rootReducer, composeWithDevTools(
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
