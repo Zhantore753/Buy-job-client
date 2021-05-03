25f3888ba090aaab83b662bf9c019a7ec8347724
Zhantore <753gg@mail.ru>
2021-05-03 17:42:17 +0600

    some corrections

--- src/components/main/balance/Balance.jsx
+++ src/components/main/balance/Balance.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { inputBalance, outputBalance } from '../../../actions/user';
+import { setCurrentBalance } from '../../../reducers/userReducer';
 
 const Balance = () => {
     const currentUser = useSelector(state => state.user.currentUser);
